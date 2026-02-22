89a1a157db91875b1099fe701882c65bc1983e8b
zynp-abc <abacizeynep14@gmail.com>
2026-02-22 23:49:27 +0300

    Add files via upload

--- day03.sql
+++ day03.sql
@@ -122,3 +122,69 @@ WHERE urun_id BETWEEN 20 AND 30;
 SELECT * 
 FROM customers
 WHERE urun_id  NOT BETWEEN 20 AND 30;
+
+CREATE TABLE brands(
+marka_id int,
+marka_isim varchar(20),
+calisan_sayisi int
+);
+
+INSERT INTO brands VALUES(100,'Vakko',12000);
+INSERT INTO brands VALUES(101,'Pierre Cardin',18000);
+INSERT INTO brands VALUES(102,'Adidas',10000);
+INSERT INTO brands VALUES(103,'LCWakiki',21000);
+
+CREATE TABLE employees02(
+id int,
+isim varchar(50),
+sehir varchar(50),
+maas int,
+isyeri  varchar(20)
+);
+
+INSERT INTO employees02 VALUES(123456789,'Ali Seker','Istanbul',2500,'Vakko');
+INSERT INTO employees02 VALUES(234567890,'Ayse Gul','Istanbul',1500,'LCWakiki');
+INSERT INTO employees02 VALUES(345678912,'Veli Yilmaz','Ankara',3000,'Vakko');
+INSERT INTO employees02 VALUES(456789123,'Veli Yilmaz','Izmir',1000,'Pierre Cardin');
+INSERT INTO employees02 VALUES(567891234,'Veli Yilmaz','Ankara',70000,'Adidas');
+INSERT INTO employees02 VALUES(678912345,'Ayse Gul','Ankara',1500,'Pierre Cardin');
+INSERT INTO employees02 VALUES(789123456,'Ayse Yasa','Bursa',2500,'Vakko');
+
+SELECT * FROM brands;
+SELECT * FROM employees02;
+
+SELECT MAX(maas) FROM employees02; 
+SELECT MIN(maas) FROM employees02;
+SELECT SUM(maas) FROM employees02;
+SELECT AVG(maas) FROM employees02;
+SELECT ROUND(AVG(maas),2) FROM employees02;
+SELECT COUNT(*) FROM employees02;
+SELECT COUNT(id) FROM employees02;
+SELECT COUNT(*) FROM employees02 WHERE maas=2500
+
+CREATE TABLE workers(
+calisan_id char(9),
+calisan_isim varchar(50),
+calisan_dogdugu_sehir varchar(50)
+);
+
+INSERT INTO workers VALUES(123456789,'Ali Can','Istanbul');
+INSERT INTO workers VALUES(234567890,'Veli Cem','Ankara');
+INSERT INTO workers VALUES(345678901,'Mine Bulut','Izmir');
+
+SELECT * FROM workers;
+
+SELECT calisan_id AS id FROM workers;
+SELECT calisan_isim isim FROM workers;
+SELECT * FROM workers AS w;
+
+-- 1. Yol
+
+SELECT marka_isim FROM brands WHERE marka_id = 100;
+SELECT * FROM employees02 WHERE isyeri = 'Vakko';
+
+-- 2. Yol
+
+SELECT * 
+FROM employees02 
+WHERE isyeri = (SELECT marka_isim FROM brands WHERE marka_id = 100);
